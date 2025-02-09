0199d18a784af27bc35fe42d1916221439f2d57f
adarsh <aadarsh.22.2003@gmail.com>
2025-02-09 22:02:06 +0530

    initial commit

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ import { app } from "./app.js";
 import { connectDB } from "./Config/dataBase.js";
 import { initializeSocket } from "./utils/Socket.js";
 import http from "http";
+import cors from "cors";
 
-dotenv.config({
-  path: "./.env",
-});
+dotenv.config(); // Remove path since .env is in root directory
+
+// Add CORS middleware
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
 
 const server = http.createServer(app);
-
 connectDB();
-
 initializeSocket(server);
 
 server.listen(process.env.PORT, () => {
